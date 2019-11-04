5145ff6456d2f721c77422546a9b00f8908321c4
Felix Finkbeiner <felix@semi.technology>
2019-11-04 11:03:31 +0100

    Add minimum python version

--- setup.py
+++ setup.py
@@ -1,11 +1,6 @@
 from setuptools import setup
 
 
-# REQUIRES = [
-#     "requests>=2.22.0",
-#     "validators>=0.14.0",
-# ]
-
 setup(name="weaviate-client",
       version="0.1.0",
       description="A python native weaviate client",
@@ -19,6 +14,7 @@ setup(name="weaviate-client",
       author="SeMI Technologies",
       author_email="hello@semi.technology",
       packages=["weaviate", "weaviate.connect"],
+      python_requires='>3.7.0',
       install_requires=[
         "requests>=2.22.0",
         "validators>=0.14.0",]),
