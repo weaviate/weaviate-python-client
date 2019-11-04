855be8fd0a63cbbd74377646db2341b3c7160843
Felix Finkbeiner <felix@semi.technology>
2019-11-04 10:17:18 +0100

    Autogenerate docs on sperate branch

--- ci/generate_doc.bash
+++ ci/generate_doc.bash
@@ -1,10 +1,10 @@
 
 echo "Go to documentation branch"
 git checkout documentation
-echo "Build documentation $(pwd)"
-make -C documentation html
 echo "Get current code"
 git merge master
+echo "Build documentation $(pwd)"
+make -C documentation html
 
 echo "See if something is to commint"
 git diff --exit-code
@@ -12,6 +12,7 @@ if [ $? -eq 0 ]; then
     echo "Documentation has not changed"
     exit 0
 fi
+
 echo "Documentation has changed build new one"
 git add documentation
 git commit -m "Autogenerating documentation 🤖"
