8f51dab18f822af35a2a01a67bb20bdceec877c5
Felix Finkbeiner <felix@semi.technology>
2019-11-04 12:12:17 +0100

    Add markdown readme to setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'readme.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(name="weaviate-client",
       version="0.1.0",
       description="A python native weaviate client",
-      long_description="""
-      A client to interact with weaviate. 
-      It allows to create schemas and classes and includes some other basic features of the weaviate REST API.
-      Weaviate may be found here: https://github.com/semi-technologies/weaviate
-      For bug reports and feature requests please use GitHub issues: 
-      https://github.com/semi-technologies/weaviate-python-client/issues      
-      """,
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author="SeMI Technologies",
       author_email="hello@semi.technology",
       packages=["weaviate", "weaviate.connect"],
