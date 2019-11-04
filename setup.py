2f26a2fe785f216d2464a1a26df586e114d79f7c
Felix Finkbeiner <felix@semi.technology>
2019-11-04 09:11:24 +0100

    Add more package information

--- setup.py
+++ setup.py
@@ -7,8 +7,17 @@ from setuptools import setup
 # ]
 
 setup(name="weaviate-client",
-      version="0.0.0",
-      description="A python weaviate client",
+      version="0.1.0",
+      description="A python native weaviate client",
+      long_description="""
+      A client to interact with weaviate. 
+      It allows to create schemas and classes and includes some other basic features of the weaviate REST API.
+      Weaviate may be found here: https://github.com/semi-technologies/weaviate
+      For bug reports and feature requests please use GitHub issues: 
+      https://github.com/semi-technologies/weaviate-python-client/issues      
+      """,
+      author="SeMI Technologies",
+      author_email="hello@semi.technology",
       packages=["weaviate", "weaviate.connect"],
       install_requires=[
         "requests>=2.22.0",
