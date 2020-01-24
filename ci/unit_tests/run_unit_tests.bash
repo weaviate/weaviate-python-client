f3c05004169a9cc65b935d4a48360a6edada0075
Felix Finkbeiner <felix@semi.technology>
2020-01-24 10:33:11 +0100

    Move unit test to script test

---
+++ ci/unit_tests/run_unit_tests.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+python -m unittest test.add_thing
+python -m unittest test.add_thing_batch
+python -m unittest test.c11y
+python -m unittest test.client
+python -m unittest test.delete
+python -m unittest test.exceptions
+python -m unittest test.is_reachable
+python -m unittest test.patch_thing
+python -m unittest test.schema
