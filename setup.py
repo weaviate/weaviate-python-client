58510e39f13512833cb549f2963a8b6e91b7d441
Felix Finkbeiner <felix@semi.technology>
2020-04-21 12:29:11 +0200

    Change version for new release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'readme.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name="weaviate-client",
-      version="0.3.3", #rc0
+      version="0.3.4", #rc0
       description="A python native weaviate client",
       long_description=long_description,
       long_description_content_type='text/markdown',
