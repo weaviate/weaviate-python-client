e288e64735a22eb6e95455e897ea674a2c29ef6c
Felix Finkbeiner <felix@semi.technology>
2020-07-27 11:14:50 +0200

    Fix renaming error in integration test

--- integration/client_functions.py
+++ integration/client_functions.py
@@ -46,23 +46,23 @@ w.create_things_in_batch(things_batch)
 w.create_actions_in_batch(actions_batch)
 
 print("Load a single things and actions")
-w._create({"name": "Andrew S. Tanenbaum"}, "Person", "28954261-0449-57a2-ade5-e9e08d11f51a")
-w._create({"name": "Alan Turing"}, "Person", "1c9cd584-88fe-5010-83d0-017cb3fcb446")
-w._create({"name": "John von Neumann"}, "Person", "b36268d4-a6b5-5274-985f-45f13ce0c642")
-w._create({"name": "Tim Berners-Lee"}, "Person", "d1e90d26-d82e-5ef8-84f6-ca67119c7998")
+w.create({"name": "Andrew S. Tanenbaum"}, "Person", "28954261-0449-57a2-ade5-e9e08d11f51a")
+w.create({"name": "Alan Turing"}, "Person", "1c9cd584-88fe-5010-83d0-017cb3fcb446")
+w.create({"name": "John von Neumann"}, "Person", "b36268d4-a6b5-5274-985f-45f13ce0c642")
+w.create({"name": "Tim Berners-Lee"}, "Person", "d1e90d26-d82e-5ef8-84f6-ca67119c7998")
 
-w._create({"name": "Legends"}, "Group", "2db436b5-0557-5016-9c5f-531412adf9c6")
-w._create({"name": "Chemists"}, "Group", "577887c1-4c6b-5594-aa62-f0c17883d9bf")
+w.create({"name": "Legends"}, "Group", "2db436b5-0557-5016-9c5f-531412adf9c6")
+w.create({"name": "Chemists"}, "Group", "577887c1-4c6b-5594-aa62-f0c17883d9bf")
 
 chemists = [None]*3
-chemists[0] = w._create({"name": "Marie Curie"}, "Person")
-chemists[1] = w._create({"name": "Fritz Haber"}, "Person")
-chemists[2] = w._create({"name": "Walter White"}, "Person")
+chemists[0] = w.create({"name": "Marie Curie"}, "Person")
+chemists[1] = w.create({"name": "Fritz Haber"}, "Person")
+chemists[2] = w.create({"name": "Walter White"}, "Person")
 
 if not sys.version_info[0] == 2:
     # Python 2.7 is not fully supported this test will not be replaced
     local_time = datetime.now(timezone.utc).astimezone()
-    w._create({"start": local_time.isoformat()}, "Call",
+    w.create({"start": local_time.isoformat()}, "Call",
              "3ab05e06-2bb2-41d1-b5c5-e044f3aa9623", weaviate.SEMANTIC_TYPE_ACTIONS)
 
 time.sleep(1.1)  # Let weaviate refresh its index with the newly loaded things
@@ -105,11 +105,11 @@ if w.get(chemists[2]) is not None:
     exit(8)
 
 # Test delete reference
-prime_ministers_group = w._create({"name": "Prime Ministers"}, "Group")
+prime_ministers_group = w.create({"name": "Prime Ministers"}, "Group")
 prime_ministers = []
 prime_minister_names = ["Wim Kok", "Dries van Agt", "Piet de Jong"]
 for name in prime_minister_names:
-    prime_ministers.append(w._create({"name": name}, "Person"))
+    prime_ministers.append(w.create({"name": name}, "Person"))
 time.sleep(1.2)
 for prime_minister in prime_ministers:
     w.add_reference(prime_ministers_group, "members", prime_minister)
@@ -126,7 +126,7 @@ w.add_reference(prime_ministers_group, "members", prime_ministers[0])
 
 print("Test query")
 expected_name = "Sophie Scholl"
-w._create({"name": expected_name}, "Person", "594b7827-f795-40d0-aabb-5e0553953dad")
+w.create({"name": expected_name}, "Person", "594b7827-f795-40d0-aabb-5e0553953dad")
 time.sleep(2.0)
 result = w.query(gql_get_sophie_scholl)
 if result["data"]["Get"]["Things"]["Person"][0]["name"] != expected_name:
