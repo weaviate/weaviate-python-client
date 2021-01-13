3b5f07129092ec40bcc5ebfc97a73f0601bd0fdc
Stefan Bogdan <stefanbogdan180394@gmail.com>
2021-01-13 16:58:25 +0100

    Implement unittests for 'connect' module

--- test/connection/test_connection.py
+++ test/connection/test_connection.py
@@ -1,11 +1,48 @@
 import unittest
+from unittest.mock import patch
+import weaviate
 
 
 class TestSum(unittest.TestCase):
 
-    def test_sum(self):
-        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+    def test_run_rest(self):
+        """
+        Test run rest casses.
+        """
 
+        connection = weaviate.connect.Connection("http://weaviate:1234")
 
-if __name__ == '__main__':
-    unittest.main()
+        with patch("weaviate.connect.connection.requests") as mock_run_rest:
+            mock_run_rest.get.return_value = "GET"
+            mock_run_rest.put.return_value = "PUT"
+            mock_run_rest.post.return_value = "POST"
+            mock_run_rest.patch.return_value = "PATCH"
+            mock_run_rest.delete.return_value = "DELETE"
+
+            # GET method
+            self.assertEqual(
+                connection.run_rest("path", weaviate.connect.REST_METHOD_GET, {}, {}),
+                "GET"
+            )
+            # PUT method
+            self.assertEqual(
+                connection.run_rest("path", weaviate.connect.REST_METHOD_PUT, {}, {}),
+                "PUT"
+            )
+            # POST method
+            self.assertEqual(
+                connection.run_rest("path", weaviate.connect.REST_METHOD_POST, {}, {}),
+                "POST"
+            )
+            # PATCH method
+            self.assertEqual(
+                connection.run_rest("path", weaviate.connect.REST_METHOD_PATCH, {}, {}),
+                "PATCH"
+            )
+            # DELETE method
+            self.assertEqual(
+                connection.run_rest("path", weaviate.connect.REST_METHOD_DELETE, {}, {}),
+                "DELETE"
+            )
+        
+
