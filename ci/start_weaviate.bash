951592335d545d0e3884a0d59ba171a72d508db4
Stefan Bogdan <stefanbogdan180394@gmail.com>
2021-01-20 16:02:52 +0100

    adapt CI to work with current Unittests and Integration Tests

--- ci/start_weaviate.bash
+++ ci/start_weaviate.bash
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 echo "Download the Weaviate docker-compose file"
-curl -s -o docker-compose.yml https://configuration.semi.technology/docker-compose
+curl -s -o docker-compose.yaml "https://configuration.semi.technology/docker-compose?"
 echo "Run Docker compose"
-nohup docker-compose up &
+nohup docker-compose up -d
 
 echo "Wait until weaviate is up"
 
 # pulling all images usually takes < 3 min
 # starting weaviate usuall takes < 2 min
 i="0"
-curl localhost:8080/v1/meta
+curl -s localhost:8080/v1/meta
 while [ $? -ne 0 ]; do
-  i=$[$i+10]
+  i=$[$i+5]
   echo "Sleep $i"
-  sleep 10
+  sleep 5
   if [ $i -gt 300 ]; then
     echo "Weaviate did not start in time"
     cat nohup.out
     exit 1
   fi
-  curl localhost:8080/v1/meta
+  curl -s localhost:8080/v1/meta
 done
 echo "Weaviate is up and running"
