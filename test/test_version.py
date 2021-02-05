aab3fad29e9c2631d76007600d00c973fd3b4fa3
Stefan Bogdan <stefanbogdan180394@gmail.com>
2021-02-05 13:57:15 +0100

    add test for the set version

---
+++ test/test_version.py
@@ -0,0 +1,11 @@
+import unittest
+import weaviate
+
+class TestVersion(unittest.TestCase):
+
+    def test_version(self):
+        """
+        Test the `__version__` global variable.
+        """
+
+        self.assertEqual(weaviate.__version__, "2.1.0", "Check if the version is set correctly!")
