e2bfc49dbac7243d0d82e3130ace356517d7f22a
Stefan Bogdan <stefanbogdan180394@gmail.com>
2021-03-31 11:08:29 +0200

    add examples to the docstring

--- weaviate/data/references/crud_references.py
+++ weaviate/data/references/crud_references.py
@@ -58,6 +58,66 @@ class Reference:
         to_uuid : str
             The UUID of the referenced object.
 
+        Examples
+        --------
+        Assume we have two classes, Author and Book.
+        >>> # Create the objects first
+        >>> client.data_object.create(
+        ...     data_object = {'name': 'Ray Bradbury'},
+        ...     class_name = 'Author',
+        ...     uuid = 'e067f671-1202-42c6-848b-ff4d1eb804ab'
+        ... )
+        >>> client.data_object.create(
+        ...     data_object = {'title': 'The Martian Chronicles'},
+        ...     class_name = 'Book',
+        ...     uuid = 'a9c1b714-4f8a-4b01-a930-38b046d69d2d'
+        ... )
+        >>> # Add the cross references
+        >>> ## Author -> Book
+        >>> client.data_object.reference.add(
+        ...     from_uuid = 'e067f671-1202-42c6-848b-ff4d1eb804ab', # Author UUID
+        ...     from_property_name = 'wroteBooks',
+        ...     to_uuid = 'a9c1b714-4f8a-4b01-a930-38b046d69d2d' # Book UUID
+        ... )
+        >>> client.data_object.get('e067f671-1202-42c6-848b-ff4d1eb804ab') # Author UUID
+        {
+            "additional": {},
+            "class": "Author",
+            "creationTimeUnix": 1617177700595,
+            "id": "e067f671-1202-42c6-848b-ff4d1eb804ab",
+            "lastUpdateTimeUnix": 1617177700595,
+            "properties": {
+                "name": "Ray Bradbury",
+                "wroteBooks": [
+                {
+                    "beacon": "weaviate://localhost/a9c1b714-4f8a-4b01-a930-38b046d69d2d",
+                    "href": "/v1/objects/a9c1b714-4f8a-4b01-a930-38b046d69d2d"
+                }
+                ]
+            },
+            "vectorWeights": null
+        }
+        >>> # delete the reference
+        >>> client.data_object.reference.delete(
+        ...     from_uuid = 'e067f671-1202-42c6-848b-ff4d1eb804ab', # Author UUID
+        ...     from_property_name = 'wroteBooks',
+        ...     to_uuid = 'a9c1b714-4f8a-4b01-a930-38b046d69d2d' # Book UUID
+        ... )
+        >>> >>> client.data_object.get('e067f671-1202-42c6-848b-ff4d1eb804ab') # Author UUID
+        {
+            "additional": {},
+            "class": "Author",
+            "creationTimeUnix": 1617177700595,
+            "id": "e067f671-1202-42c6-848b-ff4d1eb804ab",
+            "lastUpdateTimeUnix": 1617177864970,
+            "properties": {
+                "name": "Ray Bradbury",
+                "wroteBooks": []
+            },
+            "vectorWeights": null
+        }
+
+
         Raises
         ------
         requests.exceptions.ConnectionError
@@ -84,7 +144,7 @@ class Reference:
         ) -> None:
         """
         Allows to update all references in that property with a new set of references.
-        All old references will be replaced.
+        All old references will be deleted.
 
         Parameters
         ----------
@@ -105,6 +165,66 @@ class Reference:
             or
             ['fc7eb129-f138-457f-b727-1b29db191a67', ...]
             If `str` it is converted internaly into a list of str.
+
+        Examples
+        --------
+        You have data object 1 with reference property `hasFriends` and currently has one reference
+        to data object 7. Now you say, I want to update the references of data object 1.wroteBooks
+        to this list 3,4,9. After the update, the data object 1.wroteBooks is now 3,4,9, but no
+        longer contains 7.
+
+        >>> client.data_object.get('e067f671-1202-42c6-848b-ff4d1eb804ab') # Author UUID
+        {
+            "additional": {},
+            "class": "Author",
+            "creationTimeUnix": 1617177700595,
+            "id": "e067f671-1202-42c6-848b-ff4d1eb804ab",
+            "lastUpdateTimeUnix": 1617177700595,
+            "properties": {
+                "name": "Ray Bradbury",
+                "wroteBooks": [
+                {
+                    "beacon": "weaviate://localhost/a9c1b714-4f8a-4b01-a930-38b046d69d2d",
+                    "href": "/v1/objects/a9c1b714-4f8a-4b01-a930-38b046d69d2d"
+                }
+                ]
+            },
+            "vectorWeights": null
+        }
+        Currently there is only one `Book` reference.
+        Update all the references of the Author for property name `wroteBooks`.
+        >>> client.data_object.reference.update(
+        ...     from_uuid = 'e067f671-1202-42c6-848b-ff4d1eb804ab', # Author UUID
+        ...     from_property_name = 'wroteBooks',
+        ...     to_uuids = [
+        ...         '8429f68f-860a-49ea-a50b-1f8789515882',
+        ...         '3e2e6795-298b-47e9-a2cb-3d8a77a24d8a'
+        ...     ]
+        ... )
+        >>> client.data_object.get('e067f671-1202-42c6-848b-ff4d1eb804ab') # Author UUID
+        {
+            "additional": {},
+            "class": "Author",
+            "creationTimeUnix": 1617181292677,
+            "id": "e067f671-1202-42c6-848b-ff4d1eb804ab",
+            "lastUpdateTimeUnix": 1617181409405,
+            "properties": {
+                "name": "Ray Bradbury",
+                "wroteBooks": [
+                {
+                    "beacon": "weaviate://localhost/8429f68f-860a-49ea-a50b-1f8789515882",
+                    "href": "/v1/objects/8429f68f-860a-49ea-a50b-1f8789515882"
+                },
+                {
+                    "beacon": "weaviate://localhost/3e2e6795-298b-47e9-a2cb-3d8a77a24d8a",
+                    "href": "/v1/objects/3e2e6795-298b-47e9-a2cb-3d8a77a24d8a"
+                }
+                ]
+            },
+            "vectorWeights": null
+        }
+        All the previous references were removed and now we have only those specified in the
+        `update` method.
 
         Raises
         ------
@@ -151,6 +271,46 @@ class Reference:
             'http://localhost:8080/v1/objects/fc7eb129-f138-457f-b727-1b29db191a67'
             or
             'fc7eb129-f138-457f-b727-1b29db191a67'
+
+        Examples
+        --------
+        Assume we have two classes, Author and Book.
+        >>> # Create the objects first
+        >>> client.data_object.create(
+        ...     data_object = {'name': 'Ray Bradbury'},
+        ...     class_name = 'Author',
+        ...     uuid = 'e067f671-1202-42c6-848b-ff4d1eb804ab'
+        ... )
+        >>> client.data_object.create(
+        ...     data_object = {'title': 'The Martian Chronicles'},
+        ...     class_name = 'Book',
+        ...     uuid = 'a9c1b714-4f8a-4b01-a930-38b046d69d2d'
+        ... )
+        >>> # Add the cross references
+        >>> ## Author -> Book
+        >>> client.data_object.reference.add(
+        ...     from_uuid = 'e067f671-1202-42c6-848b-ff4d1eb804ab', # Author UUID
+        ...     from_property_name = 'wroteBooks',
+        ...     to_uuid = 'a9c1b714-4f8a-4b01-a930-38b046d69d2d' # Book UUID
+        ... )
+        >>> client.data_object.get('e067f671-1202-42c6-848b-ff4d1eb804ab') # Author UUID
+        {
+            "additional": {},
+            "class": "Author",
+            "creationTimeUnix": 1617177700595,
+            "id": "e067f671-1202-42c6-848b-ff4d1eb804ab",
+            "lastUpdateTimeUnix": 1617177700595,
+            "properties": {
+                "name": "Ray Bradbury",
+                "wroteBooks": [
+                {
+                    "beacon": "weaviate://localhost/a9c1b714-4f8a-4b01-a930-38b046d69d2d",
+                    "href": "/v1/objects/a9c1b714-4f8a-4b01-a930-38b046d69d2d"
+                }
+                ]
+            },
+            "vectorWeights": null
+        }
 
         Raises
         ------
