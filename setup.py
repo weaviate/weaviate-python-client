8c5ecd73bbeb54198e945a8f4df2e33694781524
Stefan Bogdan <stefanbogdan180394@gmail.com>
2021-04-15 14:34:09 +0200

    fix setup.py and requirements.txt

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from weaviate.version import __version__
 # read the contents of your README file
 from os import path
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'readme.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name="weaviate-client",
