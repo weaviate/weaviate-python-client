6f5a8d6f5680a53fe683ffc632506703b9e268f4
Bob van Luijt <bob@semi.technology>
2022-10-01 16:06:57 +0200

    await flush on fail

--- weaviate/batch/crud_batch.py
+++ weaviate/batch/crud_batch.py
@@ -788,7 +788,7 @@ class Batch:
             # 4 threads is arbitrary, this might be made more dynamic in the future
             if self._max_threads > 4:
                 self._max_threads -= 1
-            self.flush(queue_c)
+            await self.flush(queue_c)
 
     def delete_objects(self,
             class_name: str,
