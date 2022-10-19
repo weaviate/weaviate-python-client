96650f08af7774a9e992b719631d5b299e0d2293
Dirk Kulawiak <dirk@semi.technology>
2022-10-19 10:00:05 +0200

    Use batching in stress test

--- integration/test_stress.py
+++ integration/test_stress.py
@@ -120,14 +120,26 @@ class Article:
         return DataObject({"title": self.title, "datePublished": self.datePublished}, self.class_name, self.uuid)
 
 
-def test_stress():
+@pytest.mark.parametrize("dynamic", [True, False])
+@pytest.mark.parametrize("batch_size", [1, 10, 50])
+def test_stress(batch_size, dynamic):
     client = weaviate.Client("http://localhost:8080")
     client.schema.delete_all()
     client.schema.create(schema)
+    client.batch.configure(batch_size=batch_size, dynamic=dynamic)
 
-    authors = add_authors(client, random.randint(50, 100))
-    paragraphs = add_paragraphs(client, random.randint(60, 100), authors)
-    articles = add_articles(client, random.randint(90, 200), authors, paragraphs)
+    authors = create_authors(random.randint(1000, 5000))
+    paragraphs = create_paragraphs(random.randint(1000, 5000), authors)
+    articles = create_articles(random.randint(1000, 5000), authors, paragraphs)
+
+    add_authors(client, authors)
+    add_paragraphs(client, paragraphs)
+    add_articles(client, articles)
+
+    client.batch.flush()
+    __assert_add(client, authors, authors[0].class_name)
+    __assert_add(client, paragraphs, paragraphs[0].class_name)
+    __assert_add(client, articles, articles[0].class_name)
 
     # verify references
     for article in articles:
@@ -156,9 +168,18 @@ def test_profile_stress():
     client.schema.delete_all()
     client.schema.create(schema)
 
-    authors = add_authors(client, 50)
-    paragraphs = add_paragraphs(client, 100, authors)
-    add_articles(client, 100, authors, paragraphs)
+    authors = create_authors(50)
+    paragraphs = create_paragraphs(100, authors)
+    articles = create_articles(100, authors, paragraphs)
+
+    add_authors(client, authors)
+    add_paragraphs(client, paragraphs)
+    add_articles(client, articles)
+
+    client.batch.flush()
+    __assert_add(client, authors, authors[0].class_name)
+    __assert_add(client, paragraphs, paragraphs[0].class_name)
+    __assert_add(client, articles, articles[0].class_name)
 
     client.schema.delete_all()
 
@@ -167,19 +188,46 @@ def test_benchmark_stress_test(benchmark):
     benchmark(test_profile_stress)
 
 
-def add_authors(client: weaviate.Client, num_authors: int) -> List[Author]:
+def create_authors(num_authors: int) -> List[Author]:
     authors: List[Author] = []
     for _ in range(num_authors):
         authors.append(Author(''.join(random.choices(string.ascii_uppercase + string.digits, k=random.randint(5, 15)))))
-    for author in authors:
-        data_object = author.to_data_object()
-        client.data_object.create(data_object.properties, data_object.class_name, data_object.uuid)
-    print(authors)
-    __assert_add(client, authors, authors[0].class_name)
     return authors
 
 
-def add_paragraphs(client: weaviate.Client, num_paragraphs: int, authors: List[Author]) -> List[Paragraph]:
+def add_authors(client: weaviate.Client, authors: List[Author]):
+    for author in authors:
+        data_object = author.to_data_object()
+        client.batch.add_data_object(data_object.properties, data_object.class_name, data_object.uuid)
+
+
+def add_paragraphs(client: weaviate.Client, paragraphs: List[Paragraph]):
+    for paragraph in paragraphs:
+        data_object = paragraph.to_data_object()
+        client.batch.add_data_object(data_object.properties, data_object.class_name, data_object.uuid)
+        client.batch.add_reference(str(paragraph.uuid), from_property_name="author",
+                                   to_object_uuid=str(paragraph.author.to_uuid), from_object_class_name="Paragraph",
+                                   to_object_class_name="Author")
+        if paragraph.hasParagraphs is not None:
+            client.batch.add_reference(str(paragraph.uuid), from_property_name="hasParagraphs",
+                                       to_object_uuid=str(paragraph.hasParagraphs.to_uuid),
+                                       from_object_class_name="Paragraph",
+                                       to_object_class_name="Paragraph")
+
+
+def add_articles(client: weaviate.Client, articles: List[Article]):
+    for article in articles:
+        data_object = article.to_data_object()
+        client.batch.add_data_object(data_object.properties, data_object.class_name, data_object.uuid)
+        client.batch.add_reference(str(article.uuid), from_property_name="author",
+                                   to_object_uuid=str(article.author.to_uuid), from_object_class_name="Article",
+                                   to_object_class_name="Author")
+        client.batch.add_reference(str(article.uuid), from_property_name="hasParagraphs",
+                                   to_object_uuid=str(article.hasParagraphs.to_uuid), from_object_class_name="Article",
+                                   to_object_class_name="Paragraph")
+
+
+def create_paragraphs(num_paragraphs: int, authors: List[Author]) -> List[Paragraph]:
     paragraphs: List[Paragraph] = []
     for _ in range(num_paragraphs):
         content: str = ''.join(random.choices(string.ascii_uppercase + string.digits, k=random.randint(5, 15)))
@@ -193,21 +241,10 @@ def add_paragraphs(client: weaviate.Client, num_paragraphs: int, authors: List[A
                                     Reference("Paragraph",
                                               paragraph_to_reference.uuid) if paragraph_to_reference is not None else None)
                           )
-    for paragraph in paragraphs:
-        data_object = paragraph.to_data_object()
-        client.data_object.create(data_object.properties, data_object.class_name, data_object.uuid)
-        client.data_object.reference.add(paragraph.uuid, "author",
-                                         paragraph.author.to_uuid, from_class_name="Paragraph",
-                                         to_class_name="Author")
-        if paragraph.hasParagraphs is not None:
-            client.data_object.reference.add(paragraph.uuid, "hasParagraphs",
-                                             paragraph.hasParagraphs.to_uuid, from_class_name="Paragraph",
-                                             to_class_name="Paragraph")
-    __assert_add(client, paragraphs, paragraphs[0].class_name)
     return paragraphs
 
 
-def add_articles(client: weaviate.Client, num_articles: int, authors: List[Author], paragraphs: List[Paragraph]) -> \
+def create_articles(num_articles: int, authors: List[Author], paragraphs: List[Paragraph]) -> \
         List[Article]:
     articles: List[Article] = []
     base_date: datetime.date = datetime.datetime(2023, 12, 9, 7, 1, 34)
@@ -222,16 +259,6 @@ def add_articles(client: weaviate.Client, num_articles: int, authors: List[Autho
                                 Reference("Paragraph", paragraph_to_reference.uuid))
                         )
 
-    for article in articles:
-        data_object = article.to_data_object()
-        client.data_object.create(data_object.properties, data_object.class_name, data_object.uuid)
-        client.data_object.reference.add(article.uuid, "author",
-                                         article.author.to_uuid, from_class_name="Article",
-                                         to_class_name="Author")
-        client.data_object.reference.add(article.uuid, "hasParagraphs",
-                                         article.hasParagraphs.to_uuid, from_class_name="Article",
-                                         to_class_name="Paragraph")
-    __assert_add(client, articles, articles[0].class_name)
     return articles
 
 
