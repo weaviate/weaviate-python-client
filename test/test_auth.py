7810b0ce17974d9c33a49b2f568db16dbbdb6078
Stefan Bogdan <stefanbogdan180394@gmail.com>
2022-11-02 14:28:28 +0100

    add non ASCII credential unit tests

--- test/test_auth.py
+++ test/test_auth.py
@@ -18,6 +18,15 @@ class TestAuthentication(unittest.TestCase):
         self.assertEqual(request_body["client_secret"], token)
         self.assertEqual(request_body["grant_type"], "client_credentials")
 
+        token = "testßtoken4711Ø"
+        credentials = weaviate.AuthClientCredentials(token)
+        request_body = credentials.get_credentials()
+        self.assertTrue("grant_type" in request_body)
+        self.assertTrue("client_secret" in request_body)
+
+        self.assertEqual(request_body["client_secret"], token)
+        self.assertEqual(request_body["grant_type"], "client_credentials")
+
     def test_user_password(self):
         """
         Test AuthClientPassword.
@@ -25,6 +34,15 @@ class TestAuthentication(unittest.TestCase):
 
         user = "@greenstalone"
         password = "testtoken4711"
+        credentials = weaviate.AuthClientPassword(user, password)
+        request_body = credentials.get_credentials()
+
+        self.assertEqual(request_body["username"], user)
+        self.assertEqual(request_body["password"], password)
+        self.assertEqual(request_body["grant_type"], "password")
+
+        user = "@greenstaloneÐ"
+        password = "Ænima"
         credentials = weaviate.AuthClientPassword(user, password)
         request_body = credentials.get_credentials()
 
