965bc1f611c6b8e0a08e5c2d341a64b5820bb44e
Parker Duckworth <parkerduckworth@gmail.com>
2022-12-15 11:17:34 -0600

    pass on consistency_level enum name, not int value

--- weaviate/data/crud_data.py
+++ weaviate/data/crud_data.py
@@ -510,7 +510,7 @@ class DataObject:
             params["limit"] = limit
 
         if consistency_level is not None:
-            params["consistency_level"] = consistency_level
+            params["consistency_level"] = consistency_level.name
 
         if node_name is not None:
             params["node_name"] = node_name
