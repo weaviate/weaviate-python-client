3e99775ff958e796c190d9fb4ff02bb644d8e8b2
Parker Duckworth <parkerduckworth@gmail.com>
2022-12-16 12:26:08 -0600

    fix type in comment

--- weaviate/data/crud_data.py
+++ weaviate/data/crud_data.py
@@ -445,7 +445,7 @@ class DataObject:
             STRONGLY recommended to set it with Weaviate >= 1.14.0. It will be required in future
             versions of Weaviate Server and Clients. Use None value ONLY for Weaviate < v1.14.0,
             by default None
-        consistency_level : Optional[str], optional
+        consistency_level : Optional[ConsistencyLevel], optional
             Can be one of 'ALL', 'ONE', or 'QUORUM'. Determines how many replicas must acknowledge
             a request before it is considered successful. Mutually exclusive with node_name param.
         node_name : Optional[str], optional
