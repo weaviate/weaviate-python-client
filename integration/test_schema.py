586db8ea562ae0e561f2a570e180ceb40d862923
Andrzej Liszka <andrzej.liszka@gmail.com>
2022-12-20 08:05:40 +0100

    CR improvements

--- integration/test_schema.py
+++ integration/test_schema.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import pytest
 
 import weaviate
@@ -10,30 +11,10 @@ def client():
     client.schema.delete_all()
 
 
-def test_create_class_with_explicit_replication_factor(client):
-    single_class = {
-        "class": "Barbecue",
-        "description": "Barbecue or BBQ where meat and vegetables get grilled",
-        "properties": [
-            {
-                "dataType": ["string"],
-                "description": "how hot is the BBQ in C",
-                "name": "heat",
-            },
-        ],
-        "replicationConfig": {
-            "factor": 2,
-        },
-    }
-    client.schema.create_class(single_class)
-    created_class = client.schema.get("Barbecue")
-    assert created_class["class"] == "Barbecue"
-    assert created_class["replicationConfig"]["factor"] == 2
-
-    client.schema.delete_class("Barbecue")
-
-
-def test_create_class_with_implicit_replication_factor(client):
+@pytest.mark.parametrize("replicationFactor", [None, 2])
+def test_create_class_with_implicit_and_explicit_replication_factor(
+    client, replicationFactor: Optional[int]
+):
     single_class = {
         "class": "Barbecue",
         "description": "Barbecue or BBQ where meat and vegetables get grilled",
@@ -45,9 +26,17 @@ def test_create_class_with_implicit_replication_factor(client):
             },
         ],
     }
+    if replicationFactor is None:
+        expectedFactor = 1
+    else:
+        expectedFactor = replicationFactor
+        single_class["replicationConfig"] = {
+            "factor": replicationFactor,
+        }
+
     client.schema.create_class(single_class)
     created_class = client.schema.get("Barbecue")
     assert created_class["class"] == "Barbecue"
-    assert created_class["replicationConfig"]["factor"] == 1
+    assert created_class["replicationConfig"]["factor"] == expectedFactor
 
     client.schema.delete_class("Barbecue")
