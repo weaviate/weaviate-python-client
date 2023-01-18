6d1dcf381b862b028108242831f6cd4ab022fa71
Stefan Bogdan <stefanbogdan180394@gmail.com>
2023-01-18 11:32:54 +0100

    change integration tests

--- integration/test_batch.py
+++ integration/test_batch.py
@@ -1,8 +1,32 @@
+from dataclasses import dataclass
 import uuid
+from typing import Union, Sequence, Optional
 
 import pytest
 
 import weaviate
+
+
+UUID = Union[str, uuid.UUID]
+
+
+@dataclass
+class MockNumpyTorch:
+    array: list
+
+    def squeeze(self) -> "MockNumpyTorch":
+        return self
+
+    def tolist(self) -> list:
+        return self.array
+
+
+@dataclass
+class MockTensorFlow:
+    array: list
+
+    def numpy(self) -> "MockNumpyTorch":
+        return MockNumpyTorch(self.array)
 
 
 @pytest.fixture(scope="module")
@@ -12,109 +36,35 @@ def client():
     client.schema.delete_all()
 
 
-def test_add_data_object(client: weaviate.Client):
+@pytest.mark.parametrize(
+    "vector",
+    [None, [1, 2, 3], MockNumpyTorch([1, 2, 3]), MockTensorFlow([1, 2, 3])],
+)
+@pytest.mark.parametrize("uuid", [None, uuid.uuid4(), uuid.uuid4().hex, str(uuid.uuid4())])
+def test_add_data_object(client: weaviate.Client, uuid: Optional[UUID], vector: Optional[Sequence]):
     """Test the `add_data_object` method"""
-
-    assert sum(client.batch.shape) == 0
-    assert client.batch.num_objects() == 0
-    assert client.batch.is_empty_objects() is True
-
     client.batch.add_data_object(
         data_object={},
         class_name="Test",
+        uuid=uuid,
+        vector=vector,
     )
-    assert client.batch.num_objects() == 1
-
-    client.batch.add_data_object(
-        data_object={},
-        class_name="Test",
-        uuid=uuid.uuid4(),
-    )
-    assert client.batch.num_objects() == 2
-
-    client.batch.add_data_object(
-        data_object={},
-        class_name="Test",
-        uuid=str(uuid.uuid4()),
-    )
-    assert client.batch.num_objects() == 3
-
-    client.batch.add_data_object(
-        data_object={},
-        class_name="Test",
-        vector=[1, 2, 3],
-    )
-    assert client.batch.num_objects() == 4
-
-    client.batch.add_data_object(
-        data_object={},
-        class_name="Test",
-        uuid=uuid.uuid4(),
-        vector=[1, 2, 3],
-    )
-    assert client.batch.num_objects() == 5
-
-    assert client.batch.is_empty_objects() is False
-
-    client.batch.empty_objects()
-
-    assert sum(client.batch.shape) == 0
-    assert client.batch.num_objects() == 0
-    assert client.batch.is_empty_objects() is True
 
 
-def test_add_reference(client: weaviate.Client):
+@pytest.mark.parametrize("from_object_uuid", [uuid.uuid4(), uuid.uuid4().hex, str(uuid.uuid4())])
+@pytest.mark.parametrize("to_object_uuid", [uuid.uuid4(), uuid.uuid4().hex, str(uuid.uuid4())])
+@pytest.mark.parametrize("to_object_class_name", [None, "Test2"])
+def test_add_reference(
+    client: weaviate.Client,
+    from_object_uuid: UUID,
+    to_object_uuid: UUID,
+    to_object_class_name: Optional[str],
+):
     """Test the `add_reference` method"""
-
-    assert sum(client.batch.shape) == 0
-    assert client.batch.num_references() == 0
-    assert client.batch.is_empty_references() is True
-
     client.batch.add_reference(
-        from_object_uuid=uuid.uuid4(),
+        from_object_uuid=from_object_uuid,
         from_object_class_name="Test",
         from_property_name="test",
-        to_object_uuid=uuid.uuid4(),
+        to_object_uuid=to_object_uuid,
+        to_object_class_name=to_object_class_name,
     )
-    assert client.batch.num_references() == 1
-
-    client.batch.add_reference(
-        from_object_uuid=uuid.uuid4(),
-        from_object_class_name="Test",
-        from_property_name="test",
-        to_object_uuid=str(uuid.uuid4()),
-    )
-    assert client.batch.num_references() == 2
-
-    client.batch.add_reference(
-        from_object_uuid=str(uuid.uuid4()),
-        from_object_class_name="Test",
-        from_property_name="test",
-        to_object_uuid=uuid.uuid4(),
-    )
-    assert client.batch.num_references() == 3
-
-    client.batch.add_reference(
-        from_object_uuid=str(uuid.uuid4()),
-        from_object_class_name="Test",
-        from_property_name="test",
-        to_object_uuid=str(uuid.uuid4()),
-    )
-    assert client.batch.num_references() == 4
-
-    client.batch.add_reference(
-        from_object_uuid=str(uuid.uuid4()),
-        from_object_class_name="Test",
-        from_property_name="test",
-        to_object_uuid=str(uuid.uuid4()),
-        to_object_class_name="Test2",
-    )
-    assert client.batch.num_references() == 5
-
-    assert client.batch.is_empty_references() is False
-
-    client.batch.empty_references()
-
-    assert sum(client.batch.shape) == 0
-    assert client.batch.num_references() == 0
-    assert client.batch.is_empty_references() is True
