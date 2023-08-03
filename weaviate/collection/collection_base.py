aeff802812d894321e0ac3beacec7218e0728910
Tommy Smith <tommy@weaviate.io>
2023-08-03 15:41:00 +0200

    remove method function list from class docstring

--- weaviate/collection/collection_base.py
+++ weaviate/collection/collection_base.py
@@ -19,11 +19,6 @@ class _Tenants:
     collection must have been created with multi-tenancy enabled in order to use any of these methods. This class
     should not be instantiated directly, but is available as a property of the `Collection` class under
     the `collection.tenants` class attribute.
-
-    Methods:
-        - `add`: Add the specified tenants to a collection in Weaviate.
-        - `remove`: Remove the specified tenants from a collection in Weaviate.
-        - `get`: Return all tenants currently associated with a collection in Weaviate.
     """
 
     def __init__(self, connection: Connection, name: str) -> None:
