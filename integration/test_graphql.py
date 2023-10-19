817fc8ccef0d838da45f10c13a45de7f874067fd
Tommy Smith <tommy@weaviate.io>
2023-10-19 11:28:59 +0100

    fix merge errors

--- integration/test_graphql.py
+++ integration/test_graphql.py
@@ -628,9 +628,7 @@ def test_graphql_with_tenant():
 
 
 def test_graphql_with_nested_object():
-    client = weaviate.Client(
-        weaviate.ConnectionParams.from_connection_string("http://localhost:8080")
-    )
+    client = weaviate.Client("http://localhost:8080")
     client.schema.delete_all()
     client.schema.create_class(
         {
