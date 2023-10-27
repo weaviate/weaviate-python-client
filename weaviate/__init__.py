83f6e5aa55c946d4911d7fe0211befa253d42821
Tommy Smith <tommy@weaviate.io>
2023-10-27 09:59:46 +0100

    prune module init

--- weaviate/__init__.py
+++ weaviate/__init__.py
@@ -1,34 +1,5 @@
 """
 Weaviate Python Client Library used to interact with a Weaviate instance.
-
-The interaction with Weaviate instance should be through a `Client` object. A `Client` instance
-has instance attributes to all the object needed to create objects/schema, do classification,
-upload batches, query data, ... Creating separate `Schema`, `DataObject`, `Batch`,
-`Classification`, `Query`, `Connect`, `Reference` is **STRONGLY DISCOURAGED**. The `Client` class
-creates the needed instances and connects all of them to the same Weaviate instance for you.
-
-Examples
---------
-Creating and exploring a Weaviate instance running on `localhost`, on port `8080`, with Authentication disabled.
-
->>> import weaviate
->>> client = weaviate.Client('http://localhost:8080')
->>> print_type = lambda obj: print(type(obj))
->>> print_type(client.batch)
-<class 'weaviate.batch.crud_batch.Batch'>
->>> print_type(client.schema)
-<class 'weaviate.schema.crud_schema.Schema'>
->>> print_type(client.classification)
-<class 'weaviate.classification.classify.Classification'>
->>> print_type(client.data_object)
-<class 'weaviate.data.crud_data.DataObject'>
->>> print_type(client.query)
-<class 'weaviate.gql.query.Query'>
-
-Attributes
-----------
-__version__ : str
-    Current `weaviate-python` library version installed.
 """
 
 
@@ -45,18 +16,12 @@ __all__ = [
     "AuthenticationFailedException",
     "SchemaValidationException",
     "WeaviateStartUpError",
-    "ConsistencyLevel",
-    "WeaviateErrorRetryConf",
     "EmbeddedOptions",
     "AdditionalConfig",
     "Config",
     "ConnectionConfig",
     "ConnectionParams",
     "ProtocolParams",
-    "AdditionalProperties",
-    "LinkTo",
-    "Tenant",
-    "TenantActivityStatus",
     "connect_to_custom",
     "connect_to_embedded",
     "connect_to_local",
@@ -73,7 +38,6 @@ except PackageNotFoundError:
     __version__ = "unknown version"
 
 from .auth import AuthClientCredentials, AuthClientPassword, AuthBearerToken, AuthApiKey
-from .batch.crud_batch import WeaviateErrorRetryConf
 from .client import Client, WeaviateClient
 from .connect.connection import ConnectionParams, ProtocolParams
 from .connect.helpers import (
@@ -82,8 +46,6 @@ from .connect.helpers import (
     connect_to_local,
     connect_to_wcs,
 )
-from .data.replication import ConsistencyLevel
-from .schema.crud_schema import Tenant, TenantActivityStatus
 from .embedded import EmbeddedOptions
 from .exceptions import (
     UnexpectedStatusCodeException,
@@ -93,7 +55,6 @@ from .exceptions import (
     WeaviateStartUpError,
 )
 from .config import AdditionalConfig, Config, ConnectionConfig
-from .gql.get import AdditionalProperties, LinkTo
 
 if not sys.warnoptions:
     import warnings
