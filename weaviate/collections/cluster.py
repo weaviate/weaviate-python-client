81dd9014f8946aea29003d6770ffc1e1a082a702
Dirk Kulawiak <dirk@semi.technology>
2024-01-28 06:49:20 +0100

    Remove connection error from cluster

--- weaviate/collections/cluster.py
+++ weaviate/collections/cluster.py
@@ -1,7 +1,5 @@
 from typing import List, Literal, Optional, Union, overload
 
-from httpx import ConnectError
-
 from weaviate.collections.classes.cluster import Node, Shards, _ConvertFromREST
 from weaviate.connect import ConnectionV4
 from weaviate.exceptions import (
@@ -72,11 +70,7 @@ class _Cluster:
         if output is not None:
             path += f"?output={output}"
 
-        try:
-            response = self._connection.get(path=path)
-        except ConnectError as conn_err:
-            raise ConnectError("Get nodes status failed due to connection error") from conn_err
-
+        response = self._connection.get(path=path, error_msg="Get nodes status failed")
         response_typed = _decode_json_response_dict(response, "Nodes status")
         assert response_typed is not None
 
