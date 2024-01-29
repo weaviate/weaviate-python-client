1a551c814ef6e03d06b3f68e8f8ba6c01a6e21e4
Dirk Kulawiak <dirk@semi.technology>
2024-01-29 05:05:26 +0100

    Add tests for filters and fix count_by_ref bug

--- test/collection/test_classes.py
+++ test/collection/test_classes.py
@@ -5,19 +5,32 @@ from weaviate.collections.classes.grpc import (
     QueryReference,
     _QueryReferenceMultiTarget,
     _QueryReference,
+    Sort,
 )
 
+from weaviate.collections.classes.filters import Filter
 
-def test_link_to_errors_on_extra_variable():
+
+def test_link_to_errors_on_extra_variable() -> None:
     with pytest.raises(ValidationError):
         QueryReference(link_on="ref", return_property="name")
 
 
-def test_link_to_multi_target_errors_on_extra_variable():
+def test_link_to_multi_target_errors_on_extra_variable() -> None:
     with pytest.raises(ValidationError):
         _QueryReferenceMultiTarget(link_on="ref", target_colection="Test")
 
 
-def test_link_to_multi_target_is_link_to():
+def test_link_to_multi_target_is_link_to() -> None:
     link_to = _QueryReferenceMultiTarget(link_on="ref", target_collection="Test")
     assert isinstance(link_to, _QueryReference)
+
+
+def test_direct_init_filter() -> None:
+    with pytest.raises(TypeError):
+        Filter()
+
+
+def test_direct_init_sort() -> None:
+    with pytest.raises(TypeError):
+        Sort()
