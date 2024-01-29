2ff85f7b04fce88c87923944eacd5973a1e13082
Tommy Smith <tommy@weaviate.io>
2024-01-29 16:27:38 +0000

    update query docstring to include MetadataQuery.full()

--- weaviate/collections/queries/near_object/query.py
+++ weaviate/collections/queries/near_object/query.py
@@ -66,7 +66,7 @@ class _NearObjectQuery(Generic[Properties, References], _BaseQuery[Properties, R
 
         NOTE:
             - If `return_properties` is not provided then all properties are returned except for blob properties.
-            - If `return_metadata` is not provided then no metadata is provided.
+            - If `return_metadata` is not provided then no metadata is provided. Use MetadataQuery.full() to retrieve all metadata.
             - If `return_references` is not provided then no references are provided.
 
         Returns:
