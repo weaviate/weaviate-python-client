b44a11bdef3c8d50c2496ae5f8e76cd81e968cb3
Dirk Kulawiak <dirk@semi.technology>
2024-01-31 13:25:28 +0100

    Remove ID workaround + increase minimum version

--- weaviate/embedded.py
+++ weaviate/embedded.py
@@ -34,7 +34,7 @@ DEFAULT_GRPC_PORT = 50060
 class EmbeddedOptions:
     persistence_data_path: str = os.environ.get("XDG_DATA_HOME", DEFAULT_PERSISTENCE_DATA_PATH)
     binary_path: str = os.environ.get("XDG_CACHE_HOME", DEFAULT_BINARY_PATH)
-    version: str = "1.23.6"
+    version: str = "1.23.7"
     port: int = DEFAULT_PORT
     hostname: str = "127.0.0.1"
     additional_env_vars: Optional[Dict[str, str]] = None
