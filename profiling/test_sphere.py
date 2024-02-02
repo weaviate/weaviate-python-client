de21ed1432bce1d8d82492fab8747f256b8ef406
Dirk Kulawiak <dirk@semi.technology>
2024-02-02 06:56:44 +0100

    Add sift and use shared factory

--- profiling/test_sphere.py
+++ profiling/test_sphere.py
@@ -1,35 +1,17 @@
 import json
-import os
 import time
 
-import pytest
-
-import weaviate
 import weaviate.classes as wvc
-from _pytest.fixtures import SubRequest
+from .conftest import get_file_path, CollectionFactory
 
 # download sphere dataset from https://weaviate.io/blog/sphere-dataset-in-weaviate#importing-sphere-with-python
 # place file in profiling folder
 
 
-def test_sphere_new(request: SubRequest) -> None:
-    sphere_file = "sphere.100k.jsonl"
-    if not os.path.exists(sphere_file) and not os.path.exists("profiling/" + sphere_file):
-        pytest.skip("data does not exist")
-    if os.path.exists("profiling/" + sphere_file):
-        sphere_file = "profiling/" + sphere_file
+def test_sphere(collection_factory: CollectionFactory) -> None:
+    sphere_file = get_file_path("sphere.100k.jsonl")
 
-    name = request.node.name
-
-    client = weaviate.connect_to_local(
-        # headers={
-        #     "X-Cohere-Api-Key": "YOUR_KEY",
-        #     "X-OpenAI-Api-Key": "YOUR_KEY",
-        # },
-    )
-    client.collections.delete(name)
-    client.collections.create(
-        name=name,
+    collection = collection_factory(
         properties=[
             wvc.config.Property(name="url", data_type=wvc.config.DataType.TEXT),
             wvc.config.Property(name="title", data_type=wvc.config.DataType.TEXT),
@@ -37,11 +19,15 @@ def test_sphere_new(request: SubRequest) -> None:
             wvc.config.Property(name="sha", data_type=wvc.config.DataType.TEXT),
         ],
         vectorizer_config=wvc.config.Configure.Vectorizer.none(),
+        # headers={
+        #     "X-Cohere-Api-Key": "YOUR_KEY",
+        #     "X-OpenAI-Api-Key": "YOUR_KEY",
+        # },
     )
     start = time.time()
 
     import_objects = 50000
-    with client.batch.dynamic() as batch:
+    with collection.batch.dynamic() as batch:
         with open(sphere_file) as jsonl_file:
             for i, jsonl in enumerate(jsonl_file):
                 if i == import_objects or batch.number_errors > 10:
@@ -49,7 +35,6 @@ def test_sphere_new(request: SubRequest) -> None:
 
                 json_parsed = json.loads(jsonl)
                 batch.add_object(
-                    collection=name,
                     properties={
                         "url": json_parsed["url"],
                         "title": json_parsed["title"],
@@ -61,7 +46,6 @@ def test_sphere_new(request: SubRequest) -> None:
                 )
                 if i % 1000 == 0:
                     print(f"Imported {i} objects")
-    assert len(client.batch.failed_objects) == 0
-    assert len(client.collections.get(name)) == import_objects
-    client.collections.delete(name)
-    print(f"{name}: Imported {import_objects} objects in {time.time() - start}")
+    assert len(collection.batch.failed_objects) == 0
+    assert len(collection) == import_objects
+    print(f"Imported {import_objects} objects in {time.time() - start}")
