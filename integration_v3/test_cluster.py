3827e69fcf5caadd3ce6aace91e2e1bb54ed78ab
Tommy Smith <tommy@weaviate.io>
2024-02-21 09:45:26 +0000

    handle new nodes API objectCount behaviour in cluster tests

--- integration_v3/test_cluster.py
+++ integration_v3/test_cluster.py
@@ -3,6 +3,7 @@ from typing import Dict, Any
 import pytest
 
 import weaviate
+from weaviate.util import parse_version_string
 
 NODE_NAME = "node1"
 NUM_OBJECT = 10
@@ -56,21 +57,27 @@ def test_get_nodes_status_with_data(client: weaviate.Client):
     for i in range(NUM_OBJECT * 2):
         client.data_object.create({"stringProp": f"object-{i}", "intProp": i}, class_name2)
 
+    # server behaviour of resp.stats.objectCount changed by # server behaviour changed by https://github.com/weaviate/weaviate/pull/4203
+
+    server_is_at_least_124 = parse_version_string(
+        client._connection._server_version
+    ) > parse_version_string("1.24")
+
     resp = client.cluster.get_nodes_status(output="verbose")
     assert len(resp) == 1
     assert "gitHash" in resp[0]
     assert resp[0]["name"] == NODE_NAME
     assert len(resp[0]["shards"]) == 2
-    assert resp[0]["stats"]["objectCount"] == NUM_OBJECT * 3
+    assert resp[0]["stats"]["objectCount"] == 0 if server_is_at_least_124 else NUM_OBJECT * 3
     assert resp[0]["stats"]["shardCount"] == 2
     assert resp[0]["status"] == "HEALTHY"
     assert "version" in resp[0]
 
     shards = sorted(resp[0]["shards"], key=lambda x: x["class"])
     assert shards[0]["class"] == class_name1
-    assert shards[0]["objectCount"] == NUM_OBJECT
+    assert shards[0]["objectCount"] == 0 if server_is_at_least_124 else NUM_OBJECT
     assert shards[1]["class"] == class_name2
-    assert shards[1]["objectCount"] == NUM_OBJECT * 2
+    assert shards[1]["objectCount"] == 0 if server_is_at_least_124 else NUM_OBJECT * 2
 
     resp = client.cluster.get_nodes_status(class_name1, output="verbose")
     assert len(resp) == 1
@@ -82,8 +89,8 @@ def test_get_nodes_status_with_data(client: weaviate.Client):
     assert "version" in resp[0]
 
     assert shards[0]["class"] == class_name1
-    assert shards[0]["objectCount"] == NUM_OBJECT
-    assert resp[0]["stats"]["objectCount"] == NUM_OBJECT
+    assert shards[0]["objectCount"] == 0 if server_is_at_least_124 else NUM_OBJECT
+    assert resp[0]["stats"]["objectCount"] == 0 if server_is_at_least_124 else NUM_OBJECT
 
     resp = client.cluster.get_nodes_status(uncap_class_name1, output="verbose")
     assert len(resp) == 1
@@ -95,5 +102,5 @@ def test_get_nodes_status_with_data(client: weaviate.Client):
     assert "version" in resp[0]
 
     assert shards[0]["class"] == class_name1
-    assert shards[0]["objectCount"] == NUM_OBJECT
-    assert resp[0]["stats"]["objectCount"] == NUM_OBJECT
+    assert shards[0]["objectCount"] == 0 if server_is_at_least_124 else NUM_OBJECT
+    assert resp[0]["stats"]["objectCount"] == 0 if server_is_at_least_124 else NUM_OBJECT
