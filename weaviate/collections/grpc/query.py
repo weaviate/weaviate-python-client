3f16f44285ea636ce932f9e4c93c5ba70962f8dd
Dirk Kulawiak <dirk@semi.technology>
2024-02-22 15:25:38 +0100

    precompute grpc headers

--- weaviate/collections/grpc/query.py
+++ weaviate/collections/grpc/query.py
@@ -7,7 +7,6 @@ from typing import (
     Optional,
     Sequence,
     Set,
-    Tuple,
     TypeVar,
     Union,
     cast,
@@ -532,21 +531,6 @@ class _QueryGRPC(_BaseGRPC):
         return self.__call()
 
     def __call(self) -> search_get_pb2.SearchReply:
-        metadata: Optional[Tuple[Tuple[str, str], ...]] = None
-        access_token = self._connection.get_current_bearer_token()
-
-        metadata_list: List[Tuple[str, str]] = []
-        if len(access_token) > 0:
-            metadata_list.append(("authorization", access_token))
-
-        if len(self._connection.additional_headers):
-            for key, val in self._connection.additional_headers.items():
-                if val is not None:
-                    metadata_list.append((key.lower(), val))
-
-        if len(metadata_list) > 0:
-            metadata = tuple(metadata_list)
-
         try:
             assert self._connection.grpc_stub is not None
             res: search_get_pb2.SearchReply  # According to PEP-0526
@@ -696,7 +680,7 @@ class _QueryGRPC(_BaseGRPC):
                         else None
                     ),
                 ),
-                metadata=metadata,
+                metadata=self._connection.grpc_headers(),
                 timeout=self._connection.timeout_config.connect,
             )
 
