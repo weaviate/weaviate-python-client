cb2e064f4a671fa6ba8bd8ac6466bd56500cf9e4
Dirk Kulawiak <dirk@semi.technology>
2024-02-28 15:59:55 +0100

    fix names in test

--- test/collection/test_filter.py
+++ test/collection/test_filter.py
@@ -23,12 +23,12 @@ def test_filter_lists() -> None:
     f1 = wvc.query.Filter.by_property("test").equal("test")
     f2 = wvc.query.Filter.by_creation_time().greater_or_equal(datetime.datetime.now())
 
-    and_list = wvc.query.Filter.combine_with_and([f1, f2])
+    and_list = wvc.query.Filter.all_of([f1, f2])
     and_direct = f1 & f2
     assert isinstance(and_list, _FilterAnd)
     assert and_list.filters == and_direct.filters
 
-    or_list = wvc.query.Filter.combine_with_or([f1, f2])
+    or_list = wvc.query.Filter.any_of([f1, f2])
     or_direct = f1 | f2
     assert isinstance(or_list, _FilterOr)
     assert or_list.filters == or_direct.filters
