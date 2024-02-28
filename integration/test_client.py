677350a9d31980a46ae9e98f8bb5a60bb28694d7
Tommy Smith <tommy@weaviate.io>
2024-02-28 17:48:32 +0000

    Use actually unused port

--- integration/test_client.py
+++ integration/test_client.py
@@ -545,7 +545,7 @@ def test_local_proxies() -> None:
     with weaviate.connect_to_local(
         additional_config=wvc.init.AdditionalConfig(
             proxies={
-                "http": "http://localhost:8099",
+                "http": "http://localhost:8084",
                 "grpc": "http://localhost:10000",
             }
         )
