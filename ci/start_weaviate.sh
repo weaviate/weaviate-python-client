f80428fdfa63a166a4944fd7ad349f4b423b29fb
Tommy Smith <tommy@weaviate.io>
2024-02-28 17:09:52 +0000

    Fix CI misconfiguration for proxy stack

--- ci/start_weaviate.sh
+++ ci/start_weaviate.sh
@@ -14,3 +14,4 @@ nohup docker-compose -f ci/docker-compose-wcs.yml up -d
 nohup docker-compose -f ci/docker-compose-generative.yml up -d
 nohup docker-compose -f ci/docker-compose-cluster.yml up -d
 nohup docker-compose -f ci/docker-compose-rerank.yml up -d
+nohup docker-compose -f ci/docker-compose-proxy.yml up -d
