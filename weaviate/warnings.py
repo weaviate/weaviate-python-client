938a6523bb5f3ff856f11f72750728eadcd790ec
Dirk Kulawiak <dirk@semi.technology>
2024-05-09 08:40:37 -0700

    Better message

--- weaviate/warnings.py
+++ weaviate/warnings.py
@@ -157,7 +157,8 @@ class _Warnings:
     def weaviate_v3_client_is_deprecated() -> None:
         warnings.warn(
             message="""Dep016: You are creating a Weaviate v3 client using ``client =  weaviate.Client(...)``, which is
-            deprecated. Consider upgrading to the new and improved v4 client instead!
+            deprecated. Consider creating a v4 (`weaviate.WeaviateClient`) client, using a `weaviate.connect_to_<method>`
+            helper function.
             See here for
                 - migrating from v3 to v4: https://weaviate.io/developers/weaviate/client-libraries/python/v3_v4_migration
                 - usage: https://weaviate.io/developers/weaviate/client-libraries/python
