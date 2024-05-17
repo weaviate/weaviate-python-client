dcd0936998379430cf71c255b8556601fdd5ce30
Dirk Kulawiak <dirk@semi.technology>
2024-05-17 13:57:17 -0700

    Add suport for other nearXXX and generate

--- weaviate/collections/queries/hybrid/generate.py
+++ weaviate/collections/queries/hybrid/generate.py
@@ -1,4 +1,4 @@
-from typing import Generic, List, Optional
+from typing import Generic, List, Optional, Union
 
 from weaviate.collections.classes.filters import (
     _Filters,
@@ -42,7 +42,7 @@ class _HybridGenerate(Generic[Properties, References], _BaseQuery[Properties, Re
         filters: Optional[_Filters] = None,
         group_by: Optional[GroupBy] = None,
         rerank: Optional[Rerank] = None,
-        target_vector: Optional[str] = None,
+        target_vector: Optional[Union[str, List[str]]] = None,
         include_vector: INCLUDE_VECTOR = False,
         return_metadata: Optional[METADATA] = None,
         return_properties: Optional[ReturnProperties[TProperties]] = None,
