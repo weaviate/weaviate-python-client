12aee669121055232c7fa1e9ca02c1690e85a8d3
Tommy Smith <tommy@weaviate.io>
2024-07-02 10:23:26 +0100

    Parametrize test to number of backslashes

--- integration/test_injection.py
+++ integration/test_injection.py
@@ -1,9 +1,15 @@
+import pytest
 import weaviate
 import requests
 import json
 
 
-def test_gql_injection() -> None:
+def injection_template(n: int) -> str:
+    return "Liver" + ("\\" * n) + '"}}){{answer}}}}{payload}#'
+
+
+@pytest.mark.parametrize("n_backslashes", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+def test_gql_injection(n_backslashes: int) -> None:
     client = weaviate.Client(url="http://localhost:8080")
     client.schema.delete_class("Question")
     client.schema.delete_class("Hacked")
@@ -46,12 +52,11 @@ def test_gql_injection() -> None:
             batch.add_data_object(data_object=properties, class_name="Hacked")
 
     injection_payload = client.query.get("Hacked", ["answer"]).build()
-    injection_template = 'Liver\\\\"}}){{answer}}}}{payload}#'
     query = client.query.get("Question", ["question", "answer", "category"]).with_where(
         {
             "path": ["answer"],
             "operator": "NotEqual",
-            "valueText": injection_template.format(payload=injection_payload[1:]),
+            "valueText": injection_template(n_backslashes).format(payload=injection_payload[1:]),
         }
     )
     res = query.do()
