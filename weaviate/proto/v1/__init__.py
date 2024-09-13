e670cf5d51514a4595ab1f03fa798d49bd30c782
Tommy Smith <tommy@weaviate.io>
2024-09-13 09:31:27 +0100

    Fix lint

--- weaviate/proto/v1/__init__.py
+++ weaviate/proto/v1/__init__.py
@@ -1,3 +1,4 @@
 import warnings
+
 warnings.filterwarnings("ignore", ".*obsolete", UserWarning, "google.protobuf.runtime_version")
 # copied from https://github.com/grpc/grpc/issues/37609#issuecomment-2328376837 to handle https://github.com/protocolbuffers/protobuf/pull/17241
