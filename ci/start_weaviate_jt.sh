a4245e1e1edd76d0487479ce4398d6a22125172c
Tommy Smith <tommy@weaviate.io>
2024-10-31 17:14:52 +0000

    Actually use a smaller compose stack for journey tests

--- ci/start_weaviate_jt.sh
+++ ci/start_weaviate_jt.sh
@@ -7,11 +7,11 @@ export WEAVIATE_VERSION=$1
 source ./ci/compose.sh
 
 echo "Stop existing session if running"
-docker compose -f ci/docker-compose.yml down --remove-orphans
+docker compose -f ci/docker-compose-async.yml down --remove-orphans
 rm -rf weaviate-data || true
 
 echo "Run Docker compose"
-docker compose -f ci/docker-compose.yml up -d
+docker compose -f ci/docker-compose-async.yml up -d
 
 echo "Wait until the container is up"
 
