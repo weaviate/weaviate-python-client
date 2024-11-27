b22dca328b07fd18d9fdd08203c668904a618265
Dirk Kulawiak <dirk@semi.technology>
2024-11-27 08:30:20 +0100

    Add support for getting own roles

--- integration/test_rbac.py
+++ integration/test_rbac.py
@@ -159,3 +159,11 @@ def test_downsert_permissions(client_factory: ClientFactory) -> None:
             assert role is None
         finally:
             client.roles.delete(role_name)
+
+
+def test_own_roles(client_factory: ClientFactory) -> None:
+    with client_factory(ports=RBAC_PORTS, auth_credentials=RBAC_AUTH_CREDS) as client:
+        if client._connection._weaviate_version.is_lower_than(1, 28, 0):
+            pytest.skip("This test requires Weaviate 1.28.0 or higher")
+        roles = client.roles.get_current_roles()
+        assert len(roles) > 0
