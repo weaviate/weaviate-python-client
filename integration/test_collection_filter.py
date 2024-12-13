faef00ef8e1159f64b136b437e8f29b3b87807d7
melsonic <madhurjyakalita2017@gmail.com>
2024-12-13 23:40:09 +0530

    updated test name

--- integration/test_collection_filter.py
+++ integration/test_collection_filter.py
@@ -827,7 +827,7 @@ def test_filter_by_ref_and_multi_ref(collection_factory: CollectionFactory) -> N
     assert objects[0].uuid == uuid23
 
 
-def test_auto_capitalize_by_ref_multi_target() -> None:
+def test_auto_capitalize_first_letter_by_ref_multi_target() -> None:
     result = Filter.by_ref_multi_target(link_on="ref1", target_collection="test")
     target_collection_stored = result._FilterByRef__target.target_collection
     assert target_collection_stored == "Test"
