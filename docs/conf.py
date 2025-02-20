99b969a8b78d594f224f3d6f99f7f944ea691b00
Stefan Bogdan <stefanbogdan180394@gmail.com>
2025-02-20 11:03:41 +0100

    add custom css for deprecated objects

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ except PackageNotFoundError:
 # -- Project information -----------------------------------------------------
 
 project = "Weaviate Python Client"
-copyright = "2021-2024, Weaviate"
+copyright = "2021-2025, Weaviate"
 author = "Weaviate"
 
 
@@ -54,8 +54,12 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.viewcode",
     "sphinx.ext.autosectionlabel",
+    "sphinx.ext.autosummary",
     "sphinxcontrib.autodoc_pydantic",
 ]
+
+# Autosummary settings
+autosummary_generate = True
 
 # Autodoc settings for pydantic
 autodoc_pydantic_model_show_json = False
@@ -90,4 +94,7 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = ["_static"]
+
+def setup(app):
+    app.add_css_file("custom.css")
