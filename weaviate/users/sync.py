05f8ab227f5bf10b1d947d37f19fb6e7ec4fe1cb
Dirk Kulawiak <dirk@semi.technology>
2025-03-07 10:52:29 +0100

    Initial support for dynamic users

--- weaviate/users/sync.py
+++ weaviate/users/sync.py
@@ -1,8 +1,21 @@
 from weaviate import syncify
 
-from weaviate.users.users import _UsersAsync
+from weaviate.users.users import _UsersAsync, _UserDBAsync, _UserOIDCAsync
+
+
+@syncify.convert
+class _UsersDB(_UserDBAsync):
+    pass
+
+
+@syncify.convert
+class _UsersOIDC(_UserOIDCAsync):
+    pass
 
 
 @syncify.convert
 class _Users(_UsersAsync):
-    pass
+    def __init__(self, connection):
+        super().__init__(connection)
+        self.db = _UsersDB(self._connection)
+        self.oidc = _UsersOIDC(self._connection)
