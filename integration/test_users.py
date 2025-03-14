fcf9909a39cb2682c054e265462e78071fce45ee
Dirk Kulawiak <dirk@semi.technology>
2025-03-14 14:59:02 +0100

    And more tests

--- integration/test_users.py
+++ integration/test_users.py
@@ -30,6 +30,22 @@ def test_get_user_roles_deprecated(client_factory: ClientFactory) -> None:
         assert len(roles) > 0
 
 
+def test_get_user_roles_db(client_factory: ClientFactory) -> None:
+    with client_factory(ports=RBAC_PORTS, auth_credentials=RBAC_AUTH_CREDS) as client:
+        if client._connection._weaviate_version.is_lower_than(1, 30, 0):
+            pytest.skip("This test requires Weaviate 1.30.0 or higher")
+        roles = client.users.db.get_assigned_roles("admin-user")
+        assert len(roles) > 0
+
+
+def test_get_user_roles_oidc(client_factory: ClientFactory) -> None:
+    with client_factory(ports=RBAC_PORTS, auth_credentials=RBAC_AUTH_CREDS) as client:
+        if client._connection._weaviate_version.is_lower_than(1, 30, 0):
+            pytest.skip("This test requires Weaviate 1.30.0 or higher")
+        roles = client.users.oidc.get_assigned_roles("admin-user")
+        assert len(roles) > 0
+
+
 def test_get_user_with_no_roles(client_factory: ClientFactory) -> None:
     with client_factory(ports=RBAC_PORTS, auth_credentials=Auth.api_key("custom-key")) as client:
         if client._connection._weaviate_version.is_lower_than(1, 28, 0):
