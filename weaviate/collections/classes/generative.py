382e9157ca2159bee79abe7efee03d5ea086f13c
Tommy Smith <tommy@weaviate.io>
2025-03-27 09:44:39 +0000

    Permit usage like `images=base64_image`

--- weaviate/collections/classes/generative.py
+++ weaviate/collections/classes/generative.py
@@ -1,5 +1,7 @@
 from collections.abc import Iterable
 from dataclasses import dataclass
+from io import BufferedReader
+from pathlib import Path
 from typing import List, Optional, Union
 
 from pydantic import AnyHttpUrl, AnyUrl, BaseModel, Field
@@ -972,7 +974,7 @@ class GenerativeParameters:
         *,
         non_blob_properties: Optional[List[str]] = None,
         image_properties: Optional[List[str]] = None,
-        images: Optional[Iterable[BLOB_INPUT]] = None,
+        images: Optional[Union[BLOB_INPUT, Iterable[BLOB_INPUT]]] = None,
         metadata: bool = False,
     ) -> _GroupedTask:
         """Create a `_GroupedTask` object for use when performing AI generation using the `generate` namespace and the `grouped_task` field."""
@@ -980,7 +982,7 @@ class GenerativeParameters:
             prompt=prompt,
             non_blob_properties=non_blob_properties,
             image_properties=image_properties,
-            images=(parse_blob(image) for image in images) if images is not None else None,
+            images=__parse_images(images),
             metadata=metadata,
         )
 
@@ -989,7 +991,7 @@ class GenerativeParameters:
         prompt: str,
         *,
         image_properties: Optional[List[str]] = None,
-        images: Optional[Iterable[BLOB_INPUT]] = None,
+        images: Optional[Union[BLOB_INPUT, Iterable[BLOB_INPUT]]] = None,
         metadata: bool = False,
         debug: bool = False,
     ) -> _SinglePrompt:
@@ -997,7 +999,17 @@ class GenerativeParameters:
         return _SinglePrompt(
             prompt=prompt,
             image_properties=image_properties,
-            images=(parse_blob(image) for image in images) if images is not None else None,
+            images=__parse_images(images),
             metadata=metadata,
             debug=debug,
         )
+
+
+def __parse_images(
+    images: Optional[Union[BLOB_INPUT, Iterable[BLOB_INPUT]]]
+) -> Optional[Iterable[str]]:
+    if isinstance(images, (str, Path, BufferedReader)):
+        return (
+            parse_blob(images) for _ in "."
+        )  # creates an Iterable[str]-compatible Generator with a single element
+    return (parse_blob(image) for image in images) if images is not None else None
