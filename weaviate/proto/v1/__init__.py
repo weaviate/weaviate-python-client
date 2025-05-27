85b831c21643be2ce5106cc6a6bef823d68f5ed6
Dirk Kulawiak <dirk@semi.technology>
2025-05-27 10:53:34 +0200

    Correct import for v6

--- weaviate/proto/v1/__init__.py
+++ weaviate/proto/v1/__init__.py
@@ -10,7 +10,6 @@ warnings.filterwarnings(
 )
 # ref: https://github.com/grpc/grpc/issues/37609 and https://github.com/protocolbuffers/protobuf/pull/17241
 
-import importlib
 from packaging import version
 import grpc_tools
 
@@ -19,22 +18,15 @@ def get_protobuf_version() -> version.Version:
     return version.parse(grpc_tools.grpc_version.VERSION)
 
 
-def get_protobuf_major_version():
-    """Get the major version of installed protobuf (4 or 5)."""
-    pb_version = get_protobuf_version()
-    if pb_version >= version.parse("1.70.0"):
-        return 6
-    elif pb_version >= version.parse("1.60.0"):
-        return 5
-    elif pb_version >= version.parse("1.50.0"):
-        return 4
-    else:
-        raise RuntimeError(f"Unsupported grpcio-tools version: {pb_version}. Only versions 1.50.0+ and <1.80 are supported.")
-
-if get_protobuf_major_version() == 5:
+pb_version = get_protobuf_version()
+if pb_version >= version.parse("1.70.0"):
+    from weaviate.proto.v1.v6.v1 import weaviate_pb2_grpc, aggregate_pb2, base_pb2, base_search_pb2, batch_delete_pb2, batch_pb2, generative_pb2, properties_pb2, search_get_pb2, tenants_pb2
+elif pb_version >= version.parse("1.60.0"):
     from weaviate.proto.v1.v5.v1 import weaviate_pb2_grpc, aggregate_pb2, base_pb2, base_search_pb2, batch_delete_pb2, batch_pb2, generative_pb2, properties_pb2, search_get_pb2, tenants_pb2
-else:
+elif pb_version >= version.parse("1.50.0"):
     from weaviate.proto.v1.v4.v1 import weaviate_pb2_grpc, aggregate_pb2, base_pb2, base_search_pb2, batch_delete_pb2, batch_pb2, generative_pb2, properties_pb2, search_get_pb2, tenants_pb2
+else:
+    raise RuntimeError(f"Unsupported grpcio-tools version: {pb_version}. Only versions 1.50.0+ and <1.80 are supported.")
 
 __all__ = [
     "aggregate_pb2", "base_pb2", "base_search_pb2", "batch_delete_pb2", "batch_pb2", "generative_pb2", "properties_pb2", "search_get_pb2", "tenants_pb2", "weaviate_pb2_grpc"
