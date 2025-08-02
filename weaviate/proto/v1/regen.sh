bdd43c4b849b26856b987a60cbc80d6d22015e5c
Dirk Kulawiak <dirk@semi.technology>
2025-08-01 17:21:16 -0700

    More grpc versions for better compatibility

--- weaviate/proto/v1/regen.sh
+++ weaviate/proto/v1/regen.sh
@@ -24,20 +24,15 @@ source "$PROTO_VENV/bin/activate"
 pip install --upgrade pip
 
 compile_protos() {
-    local version=$1
-    local output_dir="$PROJECT_ROOT/$version"
+
+
+    local pip_version=${1}
+    local version=v${1//./}
+
+    local output_dir="$PROJECT_ROOT/${version}"
 
     echo "Installing protobuf $version and grpcio-tools..."
-    if [ "$version" = "v4" ]; then
-        pip install "grpcio-tools==1.59.5"
-    elif [ "$version" = "v5" ]; then
-        pip install "grpcio-tools==1.63.0"
-    elif [ "$version" = "v6" ]; then
-        pip install "grpcio-tools==1.72.1"
-    else
-        echo "Unsupported version: $version"
-        exit 1
-    fi
+    pip install "grpcio-tools==$pip_version"
 
     echo "Compiling protos for Protobuf $version... in $output_dir"
 
@@ -65,10 +60,19 @@ compile_protos() {
     fi
 }
 
+# v4
+compile_protos "1.59.5"
+compile_protos "1.61.3"   # no earlier versions on pypi
 
-for version in "v4" "v5" "v6"; do
-   compile_protos "$version"
-done
+# v5
+compile_protos "1.63.0"
+compile_protos "1.65.1"  # .0 was yanked
+compile_protos "1.67.0"
+compile_protos "1.69.0"
+compile_protos "1.71.0"
+
+#v6
+compile_protos "1.72.1" # .0 was yanked
 
 deactivate
 rm -rf "$PROTO_VENV"
