e4b22b86b27e704ab8f617fd7a133b6d308597fb
Dirk Kulawiak <dirk@semi.technology>
2025-08-06 12:57:03 -0700

    Add support for groups

---
+++ weaviate/groups/sync.pyi
@@ -0,0 +1,26 @@
+from typing import Dict, List, Literal, Union, overload
+
+from weaviate.connect.v4 import ConnectionSync
+from weaviate.groups.base import _BaseExecutor, _GroupsOIDCExecutor
+from weaviate.rbac.models import Role, RoleBase
+
+class _GroupsOIDC(_GroupsOIDCExecutor[ConnectionSync]):
+    @overload
+    def get_assigned_roles(
+        self, *, group_id: str, include_permissions: Literal[False] = False
+    ) -> Dict[str, RoleBase]: ...
+    @overload
+    def get_assigned_roles(
+        self, *, group_id: str, include_permissions: Literal[True]
+    ) -> Dict[str, Role]: ...
+    @overload
+    def get_assigned_roles(
+        self, *, group_id: str, include_permissions: bool = False
+    ) -> Union[Dict[str, Role], Dict[str, RoleBase]]: ...
+    def assign_roles(self, *, group_id: str, role_names: Union[str, List[str]]) -> None: ...
+    def revoke_roles(self, *, group_id: str, role_names: Union[str, List[str]]) -> None: ...
+    def get_known_group_names(self) -> List[str]: ...
+
+class _Groups(_BaseExecutor[ConnectionSync]):
+    @property
+    def oidc(self) -> _GroupsOIDC: ...
