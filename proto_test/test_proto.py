9efc175d4d324b317fd2307b4fe35b84ae30e029
Dirk Kulawiak <dirk@semi.technology>
2025-08-19 12:09:05 -0700

    Fix test for wrong protos

--- proto_test/test_proto.py
+++ proto_test/test_proto.py
@@ -1,7 +1,8 @@
+from operator import contains
 import pytest
 from importlib.metadata import version as metadata_version
 from packaging import version
-from weaviate.exceptions import WeaviateConnectionError, WeaviateProtobufIncompatibility
+from weaviate.exceptions import WeaviateConnectionError
 
 
 def test_proto_import():
@@ -11,12 +12,9 @@ def test_proto_import():
     if (pb_ver >= version.parse("6.30.0") and grpc_ver < version.parse("1.72.0")) or (
         pb_ver >= version.parse("5.26.1") and grpc_ver < version.parse("1.63.0")
     ):
-        with pytest.raises(WeaviateProtobufIncompatibility):
+        with pytest.raises(Exception) as exc_info:
             import weaviate
-
-            with pytest.raises(WeaviateConnectionError):
-                with weaviate.connect_to_local() as client:
-                    client.get_meta()
+        assert contains(str(exc_info.value), "Incompatible protobuf version")
     else:
         import weaviate
 
