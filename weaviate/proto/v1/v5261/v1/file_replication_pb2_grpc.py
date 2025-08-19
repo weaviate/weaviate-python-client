851fe28c69e1012e055be875fabdee0a57b0fc25
Tommy Smith <tommy@weaviate.io>
2025-08-19 12:15:01 +0100

    Change to generate with minimum supported grpcio-tools dep for pb major ver

--- weaviate/proto/v1/v5261/v1/file_replication_pb2_grpc.py
+++ weaviate/proto/v1/v5261/v1/file_replication_pb2_grpc.py
@@ -5,8 +5,10 @@ import warnings
 
 from weaviate.proto.v1.v5261.v1 import file_replication_pb2 as v1_dot_file__replication__pb2
 
-GRPC_GENERATED_VERSION = '1.71.2'
+GRPC_GENERATED_VERSION = '1.63.0'
 GRPC_VERSION = grpc.__version__
+EXPECTED_ERROR_RELEASE = '1.65.0'
+SCHEDULED_RELEASE_DATE = 'June 25, 2024'
 _version_not_supported = False
 
 try:
@@ -16,12 +18,15 @@ except ImportError:
     _version_not_supported = True
 
 if _version_not_supported:
-    raise RuntimeError(
+    warnings.warn(
         f'The grpc package installed is at version {GRPC_VERSION},'
         + f' but the generated code in v1/file_replication_pb2_grpc.py depends on'
         + f' grpcio>={GRPC_GENERATED_VERSION}.'
         + f' Please upgrade your grpc module to grpcio>={GRPC_GENERATED_VERSION}'
         + f' or downgrade your generated code using grpcio-tools<={GRPC_VERSION}.'
+        + f' This warning will become an error in {EXPECTED_ERROR_RELEASE},'
+        + f' scheduled for release on {SCHEDULED_RELEASE_DATE}.',
+        RuntimeWarning
     )
 
 
@@ -126,7 +131,6 @@ def add_FileReplicationServiceServicer_to_server(servicer, server):
     generic_handler = grpc.method_handlers_generic_handler(
             'weaviate.v1.FileReplicationService', rpc_method_handlers)
     server.add_generic_rpc_handlers((generic_handler,))
-    server.add_registered_method_handlers('weaviate.v1.FileReplicationService', rpc_method_handlers)
 
 
  # This class is part of an EXPERIMENTAL API.
