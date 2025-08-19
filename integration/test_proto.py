74b294f1cb366de37d18020a330dbc1278d9004e
Tommy Smith <tommy@weaviate.io>
2025-08-19 18:41:53 +0100

    Remove env var drilling in test

--- integration/test_proto.py
+++ integration/test_proto.py
@@ -1,18 +1,14 @@
-import os
 import pytest
 import weaviate
+from importlib.metadata import version as metadata_version
 from packaging import version
 from weaviate.exceptions import WeaviateProtobufIncompatibility
 
 
 @pytest.mark.proto
 def test_proto_import():
-    grpc_ver_env, pb_ver_env = os.environ.get("GRPC_VER"), os.environ.get("PB_VER")
-    assert grpc_ver_env is not None, "GRPC_VER environment variable is not set"
-    assert pb_ver_env is not None, "PB_VER environment variable is not set"
-
-    grpc_ver = version.parse(grpc_ver_env)
-    pb_ver = version.parse(pb_ver_env)
+    grpc_ver = version.parse(metadata_version("grpcio"))
+    pb_ver = version.parse(metadata_version("protobuf"))
 
     if (pb_ver >= version.parse("6.30.0") and grpc_ver < version.parse("1.72.0")) or (
         pb_ver >= version.parse("5.26.1") and grpc_ver < version.parse("1.63.0")
