076d31fb08a88e1716232ff6bf63cbacd9d7a003
JP Hwang <me@jphwang.com>
2025-12-11 17:44:07 +0100

    Update url pattern

--- test/collection/test_config.py
+++ test/collection/test_config.py
@@ -1092,11 +1092,11 @@ def test_config_with_generative(
 
 TEST_CONFIG_WITH_RERANKER = [
     (
-        Configure.Reranker.cohere(model="model", base_url="https://some.base.url"),
+        Configure.Reranker.cohere(model="model", base_url="https://some.base.url/"),
         {
             "reranker-cohere": {
                 "model": "model",
-                "baseURL": "https://some.base.url",
+                "baseURL": "https://some.base.url/",
             },
         },
     ),
