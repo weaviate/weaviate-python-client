55e56f06bf0444fb4caf9f31da7b61dad10ef291
Dirk Kulawiak <dirk@semi.technology>
2026-03-09 13:28:31 +0100

    Dont overwrite version from additional headers

--- weaviate/connect/v4.py
+++ weaviate/connect/v4.py
@@ -270,7 +270,7 @@ class _ConnectionBase:
             self.__metadata_list.append(("x-weaviate-client", self._headers["X-Weaviate-Client"]))
         if len(self.additional_headers):
             for key, val in self.additional_headers.items():
-                if val is not None:
+                if val is not None and key.lower() != "x-weaviate-client":
                     self.__metadata_list.append((key.lower(), val))
 
         if self._auth is not None:
