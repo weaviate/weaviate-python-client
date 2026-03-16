7abe7f534e5976c28c6fc489111d2080489612e0
Tommy Smith <tommy@weaviate.io>
2026-03-16 15:00:29 +0000

    Fix wrong version comparison

--- weaviate/collections/grpc/shared.py
+++ weaviate/collections/grpc/shared.py
@@ -686,14 +686,14 @@ class _BaseGRPC:
             else:
                 vector_bytes = vector_bytes_tmp
 
-        is_1_36 = self._weaviate_version.is_at_least(1, 36, 0)
+        is_1_37 = self._weaviate_version.is_at_least(1, 37, 0)
         return (
             base_search_pb2.Hybrid(
                 properties=properties,
                 query=query,
-                alpha=None if is_1_36 else (alpha if alpha is not None else 0.7),
-                alpha_param=alpha if is_1_36 else None,
-                use_alpha_param=is_1_36,
+                alpha=None if is_1_37 else (alpha if alpha is not None else 0.7),
+                alpha_param=alpha if is_1_37 else None,
+                use_alpha_param=is_1_37,
                 fusion_type=(
                     cast(
                         base_search_pb2.Hybrid.FusionType,
