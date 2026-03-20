ed373755688b09d105a932d15326993ac54059f0
Tommy Smith <tommy@weaviate.io>
2026-03-20 12:30:15 +0000

    Remove test of lazy loading shards

--- integration/test_client.py
+++ integration/test_client.py
@@ -383,10 +383,7 @@ def test_client_cluster_without_lazy_shard_loading(
         ]
         assert nodes[0].shards[0].vector_queue_length == 0
         assert nodes[0].shards[0].compressed is False
-        if collection._connection._weaviate_version.is_lower_than(1, 25, 0):
-            assert nodes[0].shards[0].loaded is True
-        else:
-            assert nodes[0].shards[0].loaded is False
+        assert nodes[0].shards[0].loaded is True
     finally:
         client.collections.delete(request.node.name)
 
